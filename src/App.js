444e7d7acc5056c1107f9d1072fad7308a678f7b
Sairoden <odingandarosa@gmail.com>
2022-08-08 20:01:28 +0800

    Create checkout page along with checkout items and checkout cart functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/routes/Home";
 import Navigation from "./components/routes/Navigation";
 import Shop from "./components/routes/Shop";
 import Authentication from "./components/routes/Authentication";
+import Checkout from "./components/routes/Checkout";
 import { Routes, Route } from "react-router-dom";
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
+        <Route path="checkout" element={<Checkout />} />
       </Route>
     </Routes>
   );
--- src/components/CartDropdown.js
+++ src/components/CartDropdown.js
@@ -1,10 +1,15 @@
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import Button from "./Button";
 import CartItem from "./CartItem";
 import { CartContext } from "../context/Cart";
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+  const goToCheckoutHandler = () => {
+    navigate("/checkout");
+  };
 
   return (
     <div className="cart-dropdown-container">
@@ -17,7 +22,7 @@ const CartDropdown = () => {
           <span className="empty-message">Your cart is empty</span>
         )}
       </div>
-      <Button>GO TO CHECKOUT</Button>
+      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
     </div>
   );
 };
